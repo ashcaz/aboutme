047b92ad7c6fb09be07837b378ba48748ece7368
Ashley Casimir <ashcas@me.com>
2020-03-26 21:13:19 -0700

    added css, added HTML text questions about education, work experience and goals, fixed links to js and css pages, added a image folder for css, cleaned up js code by making better variable names and having proper spacing

--- js/app.js
+++ js/app.js
@@ -4,70 +4,82 @@
 
 var userName = prompt('What is your name?');
 console.log('This is the answer to username promt: ' + userName);
-alert('Hi ' + userName + '!');
+alert('Hi ' + userName + '! Welcome to my about me site!');
 
 /*First part of the code should be a button that they click when they are ready to take the quiz*/
 
 /*The button should then prompt the set of questions to run*/
-var a = prompt('Am I from New Jersey?').toLowerCase();//prompt question 1
+var whereFrom = prompt('Am I from New Jersey?').toLowerCase();//prompt question 1
 
-console.log(a);
+console.log('This is the answer to whereFrom promt: ' + whereFrom);
 
 /* If-else statement for correct answer*/
 
-if(a === 'yes'|| a === 'y'){
+if(whereFrom === 'yes'|| whereFrom === 'y'){
   alert('You are correct!! You sure do pay attention!');
-}else if (a === 'no'|| a === 'n'){
+}else if (whereFrom === 'no'|| whereFrom === 'n'){
   alert('WRONG!');
 }else {
   alert('Whoops! You should probably answer using yes or no');
 }
 
-var b = prompt('Is my degree in computer science?').toLowerCase(); //prompt question 2
+var degree = prompt('Is my degree in computer science?').toLowerCase(); //prompt question 2
+
+console.log('This is the answer to degree promt: ' + degree);
 
 /* If-else statement for correct answer*/
 
-if(b === 'yes'|| b === 'y'){
+if(degree === 'yes'|| degree === 'y'){
   alert('Are you sure??');
-}else if (b === 'no' || b === 'n'){
+}else if (degree === 'no' || degree === 'n'){
   alert('Absolutely right! I have a Biology dgree!');
 }else {
   alert('Please answer using yes or no');
 }
-console.log(b);
 
-var c = prompt('Did I join the Marines?').toLowerCase(); //prompt question 2
+
+var navy = prompt('Did I join the Marines?').toLowerCase(); //prompt question 3
+
+console.log('This is the answer to navy promt: ' + navy);
+
 /* If-else statement for correct answer*/
 
-if(c === 'yes'|| c === 'y'){
+if(navy === 'yes'|| navy === 'y'){
   alert('A Jarhead I was not');
-}else if (c === 'no' || c === 'n'){
+}else if (navy === 'no' || navy === 'n'){
   alert('You are good at this. I joined the worlds greatest Navy! Hooyah!!');
 }else {
   alert('Please answer using yes or no');
 }
-console.log(c);
 
-var d = prompt('Is my degree in computer science?').toLowerCase(); //prompt question 2
+
+var job = prompt('Do I work for Amazon?').toLowerCase(); //prompt question 4
+
+console.log('This is the answer to the job promt: ' + job);
+
 /* If-else statement for correct answer*/
 
-if(d === 'yes'|| d === 'y'){
-  alert('Are you sure??');
-}else if (d === 'no' || d === 'n'){
+if(job === 'yes'|| job === 'y'){
+  alert('I do not work at Amazon...yet');
+}else if (job === 'no' || job === 'n'){
   alert('Absolutely right! I have a Biology dgree!');
 }else {
   alert('Please answer using yes or no');
 }
-console.log(d);
 
-var e = prompt('Is my #1 Goal is to graduate Code Fellows?').toLowerCase(); //prompt question 2
+
+var goal = prompt('Is my #1 Goal is to graduate Code Fellows?').toLowerCase(); //prompt question 5
+
+console.log('This is the answer to the goal promt: ' + goal);
+
 /* If-else statement for correct answer*/
 
-if(e === 'yes'|| e === 'y'){
+if(goal === 'yes'|| goal === 'y'){
   alert('HELL YES it is!!!');
-}else if (e === 'no' || e === 'n'){
+}else if (goal === 'no' || goal === 'n'){
   alert('Hmmmmmmm...lets think about this one again');
 }else {
   alert('Please answer using yes or no');
 }
-console.log(e);
+
+alert('Thank you ' + userName + ' for playing and taking the time to get to know me. See you again soon!');
